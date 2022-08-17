e53f8ebedce0b3dec032c9ec67156f1da9c8ad62
Ian Low <ianlow28@gmail.com>
2022-08-17 19:58:57 +0100

    First commit

---
+++ src/DictionaryEnCy.php
@@ -0,0 +1,32 @@
+<?php
+/* ==============================================
+TO DO:
+======
+- details
+-------------------------------------------------
+DONE:
+=====
+# Changelog
+## 1.0.0 - yyyy-mm-dd
+### Changed
+- details
+### Fixed
+- details
+### Added
+- details
+================================================= */
+namespace Ianl28\DictionaryEnCy;
+class DictionaryEnCy{
+
+  //-------------------------------
+  public function __construct(){
+    echo "This is called from DictionaryEnCy->__construct()\n";
+  }//endfunc
+  //-------------------------------
+  public function testrun(){
+    echo "This is called from DictionaryEnCy->testrun()\n";
+  }//endfunc
+  //-------------------------------
+
+}//endclass 
+?>
